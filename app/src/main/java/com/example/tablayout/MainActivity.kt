00ea905aab7e572323409fe395cbfbd63aa377f3
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-06-29 23:44:01 +0530

    second feature

--- app/src/main/java/com/example/tablayout/MainActivity.kt
+++ app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.tablayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.tablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +16,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+
+//        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+//            when (position) {
+//                0 -> tab.text = "First"
+//                1 -> tab.text = "Second"
+//                2 -> tab.text = "Third"
+//            }
+//        }.attach()
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("first"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("second"))
+        //binding.tabLayout.addTab(binding.tabLayout.newTab().setText("third"))
         binding.viewPager.adapter = adapter
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> tab.text = "First"
-                1 -> tab.text = "Second"
-                2 -> tab.text = "Third"
+
+        binding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.viewPager.currentItem = tab?.position!!
             }
-        }.attach()
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
     }
 }
